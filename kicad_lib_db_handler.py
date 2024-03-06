444a794bf05a8aa2c425f3e4def801e669241f11
Mika Laitio <lamikr@gmail.com>
2024-03-06 11:52:38 -0800

    updated the license information
    Signed-off-by: Mika Laitio <lamikr@gmail.com>

--- kicad_lib_db_handler.py
+++ kicad_lib_db_handler.py
@@ -1,3 +1,10 @@
+#
+# Copyright (c) 2024 by Mika Laitio <lamikr@gmail.com>
+#
+# License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+# See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+#
+
 import sqlite3
 from sqlite3 import Error
 
